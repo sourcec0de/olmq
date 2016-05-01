94fe0557fb8a734c49e70507af503113cc3b5031
Zwb <zwb.ict@gmail.com>
2016-05-01 22:00:27 +0800

    Add newLmdbQueue

--- queue.go
+++ queue.go
@@ -1,5 +1,11 @@
 package lmq
 
+import (
+	"sync"
+
+	"github.com/bmatsuo/lmdb-go/lmdb"
+)
+
 type Queue interface {
 	Topic(name string) Topic
 	SendMessage(topic Topic, msg []Message) bool
@@ -9,12 +15,15 @@ type Queue interface {
 
 type lmdbQueue struct {
 	path   string
+	env    *lmdb.Env
 	topics map[string]*Topic
+	mu     sync.Mutex
 }
 
 func newLmdbQueue(path string) *lmdbQueue {
 	queue := &lmdbQueue{
 		path:   path,
+		env:    lmdb.NewEnv(),
 		topics: make(map[string]*Topic),
 	}
 	return queue
