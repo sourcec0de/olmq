d09f0927dbd3094054089957094b41249d074ca6
Zwb <zwb.ict@gmail.com>
2016-05-05 11:59:41 +0800

    Add TestConsumingMessage

--- queue_test.go
+++ queue_test.go
@@ -1,6 +1,7 @@
 package lmq
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"testing"
@@ -51,4 +52,30 @@ func TestSendMessage(t *testing.T) {
 	msgs[0] = []byte("hello")
 	msgs[1] = []byte("world")
 	queue.SendMessage(topic, msgs)
+}
+
+func TestConsumingMessage(t *testing.T) {
+	root, _ := os.Getwd()
+	path := fmt.Sprintf("%s/test-consumingMessage", root)
+	os.Mkdir(path, 0755)
+	defer os.RemoveAll(path)
+	queue := newLmdbQueue(path, nil)
+	if queue == nil {
+		t.Error("newLmdbQueue failed")
+	}
+	topic := queue.Topic("GetTopic", nil)
+	if topic == nil {
+		t.Error("Topic failed")
+	}
+	topic.OpenPartitionForPersisted()
+	msgs := make([]Message, 2)
+	msgs[0] = []byte("hello")
+	msgs[1] = []byte("world")
+	queue.SendMessage(topic, msgs)
+	topic.OpenPartitionForConsuming("test1")
+	rmsgs := make([]Message, 2)
+	queue.SendMessage(topic, rmsgs)
+	if !bytes.Equal(msgs[0], rmsgs[0]) || !bytes.Compare(msgs[1], rmsgs[1]) {
+		t.Error("Consuming failed")
+	}
 }
