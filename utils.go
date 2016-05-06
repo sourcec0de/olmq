b335924c2e911d57c76c7192b15ec8a72cce29fe
Zwb <zwb.ict@gmail.com>
2016-05-06 17:25:00 +0800

    Add withRecover

--- utils.go
+++ utils.go
@@ -18,3 +18,19 @@ func bytesToUInt64(buf []byte) uint64 {
 func uInt64ToString(i uint64) string {
 	return strconv.FormatUint(i, 10)
 }
+
+// fix me. move to a good place
+var PanicHandler func(interface{})
+
+func withRecover(fn func()) {
+	defer func() {
+		handler := PanicHandler
+		if handler != nil {
+			if err := recover(); err != nil {
+				handler(err)
+			}
+		}
+	}()
+
+	fn()
+}
