bdfc4e64ae80052699c10833a5be297a771bee31
Zwb <zwb.ict@gmail.com>
2016-05-16 16:02:24 +0800

    Add test

--- async_producer_test.go
+++ async_producer_test.go
@@ -122,3 +122,119 @@ func TestNewAsyncProducerWithMultiThread(t *testing.T) {
 	}
 	<-quit
 }
+
+func BenchmarkNewAsyncProducer(b *testing.B) {
+	conf := &Config{}
+	conf.Topic.maxNum = 256
+	conf.Topic.mapSize = 256 * 1024 * 1024
+	conf.Topic.partitionSize = 1024 * 1024 * 1024
+	conf.Topic.partitionsToKeep = 8
+	conf.ChannelBufferSize = 256
+
+	root, _ := os.Getwd()
+	path := fmt.Sprintf("%s/test-newAsyncProducer", root)
+	_ = os.Mkdir(path, 0755)
+	defer func() {
+		_ = os.RemoveAll(path)
+	}()
+	producer, err := NewAsyncProducer(path, conf)
+	if producer == nil || err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		producer.Input() <- &ProducerMessage{
+			Topic:   "Topic1",
+			payload: "hello",
+		}
+	}
+
+	recvd := 0
+	results := producer.Successes()
+	for recvMsg := range results {
+		if !strings.EqualFold(recvMsg.payload, "hello") {
+			b.Error(recvMsg.payload)
+		}
+		recvd++
+		if recvd == 10 {
+			break
+		}
+	}
+	if recvd != 10 {
+		b.Error("Data lost")
+	}
+}
+
+func BenchmarkNewAsyncProducerWithMultiThread(b *testing.B) {
+	conf := &Config{}
+	conf.Topic.maxNum = 256
+	conf.Topic.mapSize = 256 * 1024 * 1024
+	conf.Topic.partitionSize = 1024 * 1024 * 1024
+	conf.Topic.partitionsToKeep = 8
+	conf.ChannelBufferSize = 256
+
+	root, _ := os.Getwd()
+	path := fmt.Sprintf("%s/test-newAsyncProducer", root)
+	_ = os.Mkdir(path, 0755)
+	defer func() {
+		_ = os.RemoveAll(path)
+	}()
+
+	quit := make(chan bool)
+	go func() {
+		producer, err := NewAsyncProducer(path, conf)
+		if producer == nil || err != nil {
+			b.Fatal(err)
+		}
+
+		for i := 0; i < 10; i++ {
+			producer.Input() <- &ProducerMessage{
+				Topic:   "Topic1",
+				payload: "hello",
+			}
+		}
+
+		recvd := 0
+		results := producer.Successes()
+		for recvMsg := range results {
+			if !strings.EqualFold(recvMsg.payload, "hello") {
+				b.Error(recvMsg.payload)
+			}
+			recvd++
+			if recvd == 10 {
+				break
+			}
+		}
+		if recvd != 10 {
+			b.Error("Data lost")
+		}
+		quit <- true
+	}()
+	producer, err := NewAsyncProducer(path, conf)
+	if producer == nil || err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		producer.Input() <- &ProducerMessage{
+			Topic:   "Topic1",
+			payload: "hello",
+		}
+	}
+
+	recvd := 0
+	results := producer.Successes()
+	for recvMsg := range results {
+		if !strings.EqualFold(recvMsg.payload, "hello") {
+			b.Error(recvMsg.payload)
+		}
+		recvd++
+		if recvd == 10 {
+			break
+		}
+	}
+	if recvd != 10 {
+		b.Error("Data lost")
+	}
+	<-quit
+}
--- consumer_test.go
+++ consumer_test.go
@@ -57,7 +57,6 @@ func TestNewConsumer(t *testing.T) {
 	reads := consumer.ReadMessages("Consumer1", "Topic1")
 	recvd = 0
 	for recvMsg := range reads {
-		fmt.Printf("%s\n", recvMsg)
 		if !bytes.Equal(recvMsg, []byte("hello")) {
 			t.Error(recvMsg)
 		}
@@ -70,3 +69,167 @@ func TestNewConsumer(t *testing.T) {
 		t.Error("Data lost")
 	}
 }
+
+func TestNewConsumerWithMultiThread(t *testing.T) {
+	conf := &Config{}
+	conf.Topic.maxNum = 256
+	conf.Topic.mapSize = 256 * 1024 * 1024
+	conf.Topic.partitionSize = 1024 * 1024 * 1024
+	conf.Topic.partitionsToKeep = 8
+	conf.ChannelBufferSize = 256
+
+	root, _ := os.Getwd()
+	path := fmt.Sprintf("%s/test-newConsumer", root)
+	_ = os.Mkdir(path, 0755)
+	defer func() {
+		_ = os.RemoveAll(path)
+	}()
+	producer, err := NewAsyncProducer(path, conf)
+	if producer == nil || err != nil {
+		t.Fatal(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		producer.Input() <- &ProducerMessage{
+			Topic:   "Topic1",
+			payload: "hello",
+		}
+	}
+
+	quit := make(chan bool)
+	go func() {
+		recvd := 0
+		results := producer.Successes()
+		for recvMsg := range results {
+			if !strings.EqualFold(recvMsg.payload, "hello") {
+				t.Error(recvMsg.payload)
+			}
+			recvd++
+			if recvd == 10 {
+				break
+			}
+		}
+		if recvd != 10 {
+			t.Error("Data lost")
+		}
+
+		consumer, err := NewConsumer(path, conf)
+		if consumer == nil || err != nil {
+			t.Fatal(err)
+		}
+
+		reads := consumer.ReadMessages("Consumer1", "Topic1")
+		recvd = 0
+		for recvMsg := range reads {
+			if !bytes.Equal(recvMsg, []byte("hello")) {
+				t.Error(recvMsg)
+			}
+			recvd++
+			if recvd == 9 {
+				break
+			}
+		}
+		if recvd != 9 {
+			t.Error("Data lost")
+		}
+		quit <- true
+	}()
+	recvd := 0
+	results := producer.Successes()
+	for recvMsg := range results {
+		if !strings.EqualFold(recvMsg.payload, "hello") {
+			t.Error(recvMsg.payload)
+		}
+		recvd++
+		if recvd == 10 {
+			break
+		}
+	}
+	if recvd != 10 {
+		t.Error("Data lost")
+	}
+
+	consumer, err := NewConsumer(path, conf)
+	if consumer == nil || err != nil {
+		t.Fatal(err)
+	}
+
+	reads := consumer.ReadMessages("Consumer1", "Topic1")
+	recvd = 0
+	for recvMsg := range reads {
+		if !bytes.Equal(recvMsg, []byte("hello")) {
+			t.Error(recvMsg)
+		}
+		recvd++
+		if recvd == 9 {
+			break
+		}
+	}
+	if recvd != 9 {
+		t.Error("Data lost")
+	}
+}
+
+/*
+func BenchmarkNewConsumer(b *testing.B) {
+	conf := &Config{}
+	conf.Topic.maxNum = 256
+	conf.Topic.mapSize = 256 * 1024 * 1024
+	conf.Topic.partitionSize = 1024 * 1024 * 1024
+	conf.Topic.partitionsToKeep = 8
+	conf.ChannelBufferSize = 256
+
+	root, _ := os.Getwd()
+	path := fmt.Sprintf("%s/test-newConsumer", root)
+	_ = os.Mkdir(path, 0755)
+	defer func() {
+		_ = os.RemoveAll(path)
+	}()
+	producer, err := NewAsyncProducer(path, conf)
+	if producer == nil || err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		producer.Input() <- &ProducerMessage{
+			Topic:   "Topic1",
+			payload: "hello",
+		}
+	}
+
+	recvd := 0
+	results := producer.Successes()
+	for recvMsg := range results {
+		if !strings.EqualFold(recvMsg.payload, "hello") {
+			b.Error(recvMsg.payload)
+		}
+		recvd++
+		if recvd == 10 {
+			break
+		}
+	}
+	if recvd != 10 {
+		b.Error("Data lost")
+	}
+
+	consumer, err := NewConsumer(path, conf)
+	if consumer == nil || err != nil {
+		b.Fatal(err)
+	}
+
+	reads := consumer.ReadMessages("Consumer1", "Topic1")
+	recvd = 0
+	for recvMsg := range reads {
+		if !bytes.Equal(recvMsg, []byte("hello")) {
+			b.Error(recvMsg)
+		}
+		recvd++
+		if recvd == 9 {
+			break
+		}
+	}
+	if recvd != 9 {
+		b.Error("Data lost")
+	}
+}
+*/
