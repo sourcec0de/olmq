04a243e584bb5662bb524678d5a79a2b37f6c90e
Zwb <zwb.ict@gmail.com>
2016-05-19 14:27:03 +0800

    Add NewConfig

--- config.go
+++ config.go
@@ -16,5 +16,9 @@ type Config struct {
 // NewConfig returns a new configuration instance with sane defaults.
 func NewConfig() *Config {
 	c := &Config{}
+	c.Topic.maxNum = 256 * 2
+	c.ChannelBufferSize = 256
+	c.Topic.partitionSize = 1024 * 1024 * 1024 * 2
+	c.Topic.partitionsToKeep = 8
 	return c
 }
